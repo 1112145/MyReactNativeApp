b15a46f06580f1d578887cf714134bf6a320cfac
dangkhoaicao <dangkhoaicao@gmail.com>
2017-12-19 18:09:12 +0700

    styling 80%

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Image } from 'react-native';
-import { SocialIcon, Avatar } from 'react-native-elements';
+import { View, StyleSheet, Image, Text, ScrollView } from 'react-native';
+import { SocialIcon, Divider, FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
 
 const AppLogo = require('../assets/images/logo.png')
 
 class Login extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <View style={styles.avatarContainer}>
-                    <Image
-                        style={styles.logo}
-                        source={AppLogo}
-                    />
-                </View >
-                <View style={styles.authenticateContainer}>
-                    <SocialIcon
-                        title='Sign In With Facebook'
-                        button
-                        type='facebook'
-                    />
-                    <SocialIcon
-                        title='Sign In With Google Plus'
-                        button
-                        type='google-plus-official'
-                    />
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.avatarContainer}>
+                        <Image style={styles.logo} source={AppLogo} />
+                    </View >
+                    <View style={styles.localLogin}>
+                        <FormLabel>User Name</FormLabel>
+                        <FormInput placeholder='Enter user name' />
+                        <FormValidationMessage></FormValidationMessage>
+                        <FormLabel>Password</FormLabel>
+                        <FormInput placeholder='Enter password' secureTextEntry={true} />
+                        <FormValidationMessage></FormValidationMessage>
+                        <Button buttonStyle={{ marginTop: 20 }} title='Login' backgroundColor='#00BCD4' />
+                    </View>
+                    <Text style={{ textAlign: 'center' }}>OR</Text>
+                    <View style={styles.socialLogin}>
+                        <SocialIcon
+                            title='Sign In With Facebook'
+                            type='facebook'
+                        />
+                        <SocialIcon
+                            title='Sign In With Google Plus'
+                            type='google-plus-official' />
+                    </View>
                 </View>
-
-            </View>
+            </ScrollView>
         );
     }
 
@@ -39,20 +44,27 @@ class Login extends Component {
 const styles = StyleSheet.create({
     container: {
         backgroundColor: 'white',
-        padding: 20,
-        height: '100%'
+        height: '100%',
+        alignItems: 'center',
+    },
+    logo: {
+        width: 100,
+        height: 100
     },
     avatarContainer: {
         justifyContent: 'center',
         flex: 1,
         alignItems: 'center',
     },
-    authenticateContainer: {
-        flex: 1
+    localLogin: {
+        maxWidth: 512,
+        padding: 20,
     },
-    logo: {
-        width: 100,
-        height: 100
+    socialLogin: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row'
     }
 });
 

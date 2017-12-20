1bf160da0efaa68b68492eceedc770b6b9882799
dangkhoaicao <dangkhoaicao@gmail.com>
2017-12-20 12:09:06 +0700

    add sign up form

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <Login />
+      <SignUp />
     );
   }
 }
--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,26 +1,56 @@
-//import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image, Text, ScrollView } from 'react-native';
+import { SocialIcon, Divider, FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
 
-// create a component
+const AppLogo = require('../assets/images/logo.png')
+
 class SignUp extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>SignUp</Text>
-            </View>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.avatarContainer}>
+                        <Image style={styles.logo} source={AppLogo} />
+                    </View >
+                    <View style={styles.localLogin}>
+                        <FormLabel>USER NAME</FormLabel>
+                        <FormInput placeholder='Enter user name' />
+                        <FormValidationMessage></FormValidationMessage>
+                        <FormLabel>PASSWORD</FormLabel>
+                        <FormInput placeholder='Enter password' secureTextEntry={true} />
+                        <FormValidationMessage></FormValidationMessage>
+                        <FormLabel>CONFIRM PASSWORD</FormLabel>
+                        <FormInput placeholder='Enter confirm password' secureTextEntry={true} />
+                        <FormValidationMessage></FormValidationMessage>
+                        <Button buttonStyle={{ marginTop: 20 }} title='SIGN UP' backgroundColor='#F44336' />
+                    </View>
+                </View>
+            </ScrollView>
         );
     }
 }
 
-// define your styles
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
+        backgroundColor: 'white',
+        height: '100%',
         alignItems: 'center',
-        backgroundColor: '#2c3e50',
     },
+    logo: {
+        width: 50,
+        height: 50,
+        marginTop: 10
+    },
+    avatarContainer: {
+        justifyContent: 'center',
+        flex: 1,
+        alignItems: 'center',
+    },
+    signUp: {
+        maxWidth: 512,
+        padding: 20,
+    }
+    
 });
 
 //make this component available to the app

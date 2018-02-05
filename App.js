7f091fc43dc10de983afec3d8c2c61b07ad75eb2
dangkhoaicao <dangkhoaicao@gmail.com>
2018-02-05 14:30:07 +0700

    prevent home back to login after back press

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 
 import Login from './src/containers/Auth/Login';
 import SignUp from './src/containers/Auth/SignUp';
 import Home from './src/containers/Pages/Home';
-import { Router, Stack, Scene } from 'react-native-router-flux';
+import { Router, Stack, Scene, ActionConst } from 'react-native-router-flux';
 
 import { Font, AppLoading } from 'expo';
 import { MaterialIcons } from '@expo/vector-icons';
@@ -26,12 +25,22 @@ export default class App extends React.Component {
     return (
         <Router sceneStyle={styles.container}>
           <Stack key="root">
-            <Scene key="login" component={Login} hideNavBar />
+            <Scene key="login" component={Login} hideNavBar  />
             <Scene key="signup" component={SignUp} hideNavBar />
-            <Scene key="home" component={Home} hideNavBar/>
+            <Scene key="home" component={Home} hideNavBar onEnter={this.checkIfLogined.bind(this)} type={ActionConst.RESET}/>
           </Stack>
         </Router>
     );
+  }
+
+  checkIfLogined(){
+    try {
+      const value = AsyncStorage.getItem('userData');
+      if (value == null){
+        Actions.login();
+      }
+    } catch (error) {
+    }
   }
 
   async componentWillMount() {
